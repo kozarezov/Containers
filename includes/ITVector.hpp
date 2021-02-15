70c6156b67652aed1faafe9a08edc05e20732112
kozarezov <kozarezov.dd@gmail.com>
2021-02-15 13:53:38 +0300

    4.1 Vector

--- includes/ITVector.hpp
+++ includes/ITVector.hpp
@@ -6,11 +6,506 @@
 /*   By: ceccentr <ceccentr@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/13 15:35:14 by ceccentr          #+#    #+#             */
-/*   Updated: 2021/02/13 15:35:41 by ceccentr         ###   ########.fr       */
+/*   Updated: 2021/02/15 13:45:22 by ceccentr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ITVECTOR_HPP
 # define ITVECTOR_HPP
+
+# include <iostream>
+
+namespace ft
+{
+	template <typename T>
+	class ITVector
+	{
+	public:
+		typedef std::ptrdiff_t difference_type;
+
+	private:
+		T *ptr;
+	public:
+		explicit ITVector() : ptr(nullptr)
+		{
+		};
+
+		explicit ITVector(T *ptr) : ptr(ptr)
+		{
+		};
+
+		ITVector(ITVector const &other)
+		{
+			*this = other;
+		};
+
+		ITVector &operator=(ITVector const &other)
+		{
+			if (this != &other)
+				this->ptr = other.ptr;
+			return (*this);
+		};
+
+		~ITVector()
+		{
+		};
+
+		T *getPtr() const
+		{
+			return (this->ptr);
+		};
+
+		ITVector &operator++()
+		{
+			++this->ptr;
+			return (*this);
+		};
+
+		ITVector &operator--()
+		{
+			--this->ptr;
+			return (*this);
+		};
+
+		ITVector operator++(int) const
+		{
+			ITVector tmp = *this;
+			
+			++this->ptr;
+			return (tmp);
+		};
+
+		ITVector operator--(int) const
+		{
+			ITVector tmp = *this;
+
+			--this->ptr;
+			return (*this);
+		};
+
+		bool operator==(ITVector const &other)
+		{
+			return (this->ptr == other.ptr);
+		};
+
+		bool operator!=(ITVector const &other)
+		{
+			return (this->ptr != other.ptr);
+		};
+
+		ITVector operator+(difference_type n) const
+		{
+			return (ITVector(this->ptr + n));
+		};
+
+		ITVector operator-(difference_type n) const
+		{
+			return (ITVector(this->ptr - n));
+		};
+
+		difference_type operator-(const ITVector &it) const
+		{
+			return (ITVector(this->ptr-it.ptr));
+		};
+
+		ITVector &operator-=(difference_type n)
+		{
+			this->ptr -= n;
+			return (*this);
+		};
+
+		ITVector &operator+=(difference_type n)
+		{
+			this->ptr += n;
+			return (*this);
+		};
+
+		T &operator[](difference_type n) const
+		{
+			return (this->ptr[n]);
+		};
+
+		T &operator*()
+		{
+			return (*(this->ptr));
+		};
+
+		T *operator->()
+		{
+			return (this->ptr);
+		};
+	};
+
+	template <typename T>
+	class ConstITVector
+	{
+	public:
+		typedef std::ptrdiff_t difference_type;
+
+	private:
+		T *ptr;
+	public:
+		explicit ConstITVector() : ptr(nullptr)
+		{
+		};
+
+		explicit ConstITVector(T *ptr) : ptr(ptr)
+		{
+		};
+
+		ConstITVector(ConstITVector const &other)
+		{
+			*this = other;
+		};
+
+		ConstITVector(ITVector<T> const &other)
+		{
+			*this = other;
+		};
+
+		ConstITVector &operator=(ConstITVector const &other)
+		{
+			if (this != &other)
+				this->ptr = other.ptr;
+			return (*this);
+		};
+
+		ConstITVector &operator=(ITVector<T> const &other)
+		{
+			if (this != &other)
+				this->ptr = other.ptr;
+			return (*this);
+		};
+
+		~ConstITVector()
+		{
+		};
+
+		ConstITVector &operator++()
+		{
+			++this->ptr;
+			return (*this);
+		};
+
+		ConstITVector &operator--()
+		{
+			--this->ptr;
+			return (*this);
+		};
+
+		ConstITVector operator++(int) const
+		{
+			ConstITVector tmp = *this;
+			
+			++this->ptr;
+			return (tmp);
+		};
+
+		ConstITVector operator--(int) const
+		{
+			ConstITVector tmp = *this;
+
+			--this->ptr;
+			return (*this);
+		};
+
+		bool operator==(ConstITVector const &other)
+		{
+			return (this->ptr == other.ptr);
+		};
+
+		bool operator!=(ConstITVector const &other)
+		{
+			return (this->ptr != other.ptr);
+		};
+
+		ConstITVector operator+(difference_type n) const
+		{
+			return (ConstITVector(this->ptr + n));
+		};
+
+		ConstITVector operator-(difference_type n) const
+		{
+			return (ConstITVector(this->ptr - n));
+		};
+
+		difference_type operator-(const ConstITVector &it) const
+		{
+			return (ConstITVector(this->ptr-it.ptr));
+		};
+
+		ConstITVector &operator-=(difference_type n)
+		{
+			this->ptr -= n;
+			return (*this);
+		};
+
+		ConstITVector &operator+=(difference_type n)
+		{
+			this->ptr += n;
+			return (*this);
+		};
+
+		T &operator[](difference_type n) const
+		{
+			return (this->ptr[n]);
+		};
+
+		const T &operator*()
+		{
+			return (*(this->ptr));
+		};
+
+		T *operator->()
+		{
+			return (this->ptr);
+		};
+	};
+
+		template <typename T>
+	class ReverseITVector
+	{
+	public:
+		typedef std::ptrdiff_t difference_type;
+		
+	private:
+		T *ptr;
+	public:
+		explicit ReverseITVector() : ptr(nullptr)
+		{
+		};
+
+		explicit ReverseITVector(T *ptr) : ptr(ptr)
+		{
+		};
+
+		ReverseITVector(ReverseITVector const &other)
+		{
+			*this = other;
+		};
+
+		ReverseITVector &operator=(ReverseITVector const &other)
+		{
+			if (this != &other)
+				this->ptr = other.ptr;
+			return (*this);
+		};
+
+		~ReverseITVector()
+		{
+		};
+
+		T *getPtr() const
+		{
+			return (this->ptr);
+		};
+
+		ReverseITVector &operator++()
+		{
+			--this->ptr;
+			return (*this);
+		};
+
+		ReverseITVector &operator--()
+		{
+			++this->ptr;
+			return (*this);
+		};
+
+		ReverseITVector operator++(int) const
+		{
+			ReverseITVector tmp = *this;
+			
+			--this->ptr;
+			return (tmp);
+		};
+
+		ReverseITVector operator--(int) const
+		{
+			ReverseITVector tmp = *this;
+
+			++this->ptr;
+			return (*this);
+		};
+
+		bool operator==(ReverseITVector const &other)
+		{
+			return (this->ptr == other.ptr);
+		};
+
+		bool operator!=(ReverseITVector const &other)
+		{
+			return (this->ptr != other.ptr);
+		};
+
+		ReverseITVector operator+(difference_type n) const
+		{
+			return (ReverseITVector(this->ptr - n));
+		};
+
+		ReverseITVector operator-(difference_type n) const
+		{
+			return (ITReverseITVectorVector(this->ptr + n));
+		};
+
+		difference_type operator-(const ReverseITVector &it) const
+		{
+			return (ReverseITVector(this->ptr - it.ptr));
+		};
+
+		ReverseITVector &operator-=(difference_type n)
+		{
+			this->ptr += n;
+			return (*this);
+		};
+
+		ReverseITVector &operator+=(difference_type n)
+		{
+			this->ptr -= n;
+			return (*this);
+		};
+
+		T &operator[](difference_type n) const
+		{
+			return (this->ptr[n]);
+		};
+
+		T &operator*()
+		{
+			return (*(this->ptr));
+		};
+
+		T *operator->()
+		{
+			return (this->ptr);
+		};
+	};
+
+	template <typename T>
+	class ConstReverseITVector
+	{
+	public:
+		typedef std::ptrdiff_t difference_type;
+		
+	private:
+		T *ptr;
+	public:
+		explicit ConstReverseITVector() : ptr(nullptr)
+		{
+		};
+
+		explicit ConstReverseITVector(T *ptr) : ptr(ptr)
+		{
+		};
+
+		ConstReverseITVector(ConstReverseITVector const &other)
+		{
+			*this = other;
+		};
+
+		ConstReverseITVector(ReverseITVector<T> const &other)
+		{
+			*this = other;
+		};
+
+		ConstReverseITVector &operator=(ConstReverseITVector const &other)
+		{
+			if (this != &other)
+				this->ptr = other.ptr;
+			return (*this);
+		};
+
+		ConstReverseITVector &operator=(ReverseITVector<T> const &other)
+		{
+			if (this != &other)
+				this->ptr = other.ptr;
+			return (*this);
+		};
+
+		~ConstReverseITVector()
+		{
+		};
+
+		ConstReverseITVector &operator++()
+		{
+			--this->ptr;
+			return (*this);
+		};
+
+		ConstReverseITVector &operator--()
+		{
+			++this->ptr;
+			return (*this);
+		};
+
+		ConstReverseITVector operator++(int) const
+		{
+			ConstReverseITVector tmp = *this;
+			
+			--this->ptr;
+			return (tmp);
+		};
+
+		ConstReverseITVector operator--(int) const
+		{
+			ConstReverseITVector tmp = *this;
+
+			++this->ptr;
+			return (*this);
+		};
+
+		bool operator==(ConstReverseITVector const &other)
+		{
+			return (this->ptr == other.ptr);
+		};
+
+		bool operator!=(ConstReverseITVector const &other)
+		{
+			return (this->ptr != other.ptr);
+		};
+
+		ConstReverseITVector operator+(difference_type n) const
+		{
+			return (ConstReverseITVector(this->ptr - n));
+		};
+
+		ConstReverseITVector operator-(difference_type n) const
+		{
+			return (ConstReverseITVector(this->ptr + n));
+		};
+
+		difference_type operator-(const ConstReverseITVector &it) const
+		{
+			return (ConstReverseITVector(this->ptr - it.ptr));
+		};
+
+		ConstReverseITVector &operator-=(difference_type n)
+		{
+			this->ptr += n;
+			return (*this);
+		};
+
+		ConstReverseITVector &operator+=(difference_type n)
+		{
+			this->ptr -= n;
+			return (*this);
+		};
+
+		T &operator[](difference_type n) const
+		{
+			return (this->ptr[n]);
+		};
+
+		const T &operator*()
+		{
+			return (*(this->ptr));
+		};
+
+		T *operator->()
+		{
+			return (this->ptr);
+		};
+	};
+};
 
 #endif
--- includes/Vector.hpp
+++ includes/Vector.hpp
@@ -6,11 +6,53 @@
 /*   By: ceccentr <ceccentr@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/13 15:32:00 by ceccentr          #+#    #+#             */
-/*   Updated: 2021/02/13 15:37:38 by ceccentr         ###   ########.fr       */
+/*   Updated: 2021/02/15 13:50:17 by ceccentr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef VECTOR_HPP
 # define VECTOR_HPP
+
+# include <iostream>
+# include "ITVector.hpp"
+
+/* https://en.cppreference.com/w/cpp/container/vector */
+
+namespace ft
+{
+	template <typename T, typename Alloc = std::allocator<T> >
+	class vector
+	{
+	public:
+	/* Member types */
+		typedef T											value_type;
+		typedef Alloc										allocator_type;
+		typedef std::size_t									size_type;
+		typedef std::ptrdiff_t								difference_type;
+		typedef typename allocator_type::reference			reference;
+		typedef typename allocator_type::const_reference	const_reference;
+		typedef typename allocator_type::pointer			pointer;
+		typedef typename allocator_type::const_pointer		const_pointer;
+		typedef typename ft::ITVector<T>					iterator;
+		typedef typename ft::ConstITVector<T>				const_iterator;
+		typedef typename ft::ReverseITVector<T>				reverse_iterator;
+		typedef typename ft::ConstReverseITVector<T>		const_reverse_iterator;
+
+	private:
+		size_type		_size;
+		size_type		_capacity;
+		value_type		*_ptr;
+		allocator_type	_alloc;
+
+	public:
+	/* My functions */
+
+	/* ... */
+
+	/* Member functions */
+	
+	};
+}
+
 
 #endif
